3309e6452e21c431ea3c10127062d62995c42af2
Ivan <0675361905vi@gmail.com>
2024-02-01 13:04:23 +0200

    add project

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import user from 'path/to/user.json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+
 );
